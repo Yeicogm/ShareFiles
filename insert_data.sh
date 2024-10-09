d8501ba9f6cc16deef61ad7775a4f2060de3c685
Yeicogm <98023036+Yeicogm@users.noreply.github.com>
2024-10-09 07:30:51 +0200

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-
 if [[ $1 == "test" ]]
 then
   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
 else
-
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+# Do not change code above this line. Use the PSQL variable above to query your database.
   echo $($PSQL "TRUNCATE teams, games")
 # Archivo CSV
 CSV_FILE="games.csv"
@@ -35,8 +35,3 @@ do
     $PSQL "INSERT INTO games (year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($year, '$round', $winner_id, $opponent_id, $winner_goals, $opponent_goals);"
   fi
 done < "$CSV_FILE"
-
-
-fi
-
-# Do not change code above this line. Use the PSQL variable above to query your database.
